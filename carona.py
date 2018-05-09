de8a70e3cd954443cb71becf403443d50752552c
eitchtee <netotrotta@gmail.com>
2018-05-09 02:36:28 -0300

    v1.0

--- carona.py
+++ carona.py
@@ -7,12 +7,50 @@ from tkinter import messagebox
 from selenium import common
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 
 
-# noinspection PyBroadException
+def postar_no_whatsapp(grupos, mensagem):
+    global sucesso_wp
+    print('Postando no Whatsapp')
+    options = Options()
+    # options.add_argument("--headless")
+
+    driver = webdriver.Firefox(firefox_options=options,
+                               executable_path=os.path.join(path[0], 'geckodriver.exe'))
+    driver.get("https://web.whatsapp.com/")
+
+    wait = WebDriverWait(driver, 900)
+
+    for grupo in grupos:
+        x_arg = '//span[contains(@title, ' + '"' + grupo + '"' + ')]'
+        janela_de_conversa = wait.until(ec.presence_of_element_located((
+            By.XPATH, x_arg)))
+
+        janela_de_conversa.click()
+        caixa_de_texto_caminho = \
+            '//div[@class="_2S1VP copyable-text selectable-text"][@dir="ltr"][@data-tab="1"]'
+        caixa_de_texto = wait.until(ec.presence_of_element_located((
+            By.XPATH, caixa_de_texto_caminho)))
+
+        for parte in mensagem.split('\n'):
+            caixa_de_texto.send_keys(parte)
+            ActionChains(driver).key_down(Keys.SHIFT).key_down(Keys.ENTER).key_up(
+                Keys.SHIFT).key_up(Keys.ENTER).perform()
+        caixa_de_texto.send_keys(Keys.ENTER)
+        sleep(20)
+
+    sucesso_wp = True
+    driver.close()
+
+
 def postar_no_facebook(email, senha, grupos, mensagem):
+    global sucesso_fb
+    print('Postando no Facebook')
     profile = webdriver.FirefoxProfile()
     profile.set_preference("browser.cache.disk.enable", False)
     profile.set_preference("browser.cache.memory.enable", False)
@@ -64,58 +102,13 @@ def postar_no_facebook(email, senha, grupos, mensagem):
         sleep(5)
 
     driver.close()
-    messagebox.showinfo('Sucesso!', 'Todas as caronas foram postadas.')
+    sucesso_fb = True
     return
 
 
 def carona_handler():
-    # Muriaé >> Ouro Preto
-    if origem_var.get() == 'Muriaé' and destino_var.get() == 'Ouro Preto':
-        print('Muriaé >> Ouro Preto')
-        grupos = ['https://www.facebook.com/groups/197987093613087/',
-                  'https://www.facebook.com/groups/314296848663622/',
-                  'https://www.facebook.com/groups/1487504921462576/',
-                  'https://www.facebook.com/groups/1671840656410743/']
-
-    # Ouro Preto >> Muriaé
-    elif origem_var.get() == 'Ouro Preto' and destino_var.get() == 'Muriaé':
-        print('Ouro Preto >> Muriaé')
-        grupos = ['https://www.facebook.com/groups/197987093613087/',
-                  'https://www.facebook.com/groups/314296848663622/',
-                  'https://www.facebook.com/groups/1487504921462576/',
-                  'https://www.facebook.com/groups/1671840656410743/']
-
-    # Ouro Preto >> Viçosa
-    elif origem_var.get() == 'Ouro Preto' and destino_var.get() == 'Viçosa':
-        print('Ouro Preto >> Viçosa')
-        grupos = ['https://www.facebook.com/groups/197987093613087/',
-                  'https://www.facebook.com/groups/148666088555002/',
-                  'https://www.facebook.com/groups/178889795534679/',
-                  'https://www.facebook.com/groups/233530423442784/']
-
-    # Viçosa >> Ouro Preto
-    elif origem_var.get() == 'Viçosa' and destino_var.get() == 'Ouro Preto':
-        print('Viçosa >> Ouro Preto')
-        grupos = ['https://www.facebook.com/groups/197987093613087/',
-                  'https://www.facebook.com/groups/148666088555002/',
-                  'https://www.facebook.com/groups/178889795534679/',
-                  'https://www.facebook.com/groups/233530423442784/']
-
-    # Viçosa >> Muriaé
-    elif origem_var.get() == 'Viçosa' and destino_var.get() == 'Muriaé':
-        print('Viçosa >> Muriaé')
-        grupos = ['https://www.facebook.com/groups/197987093613087/',
-                  'https://www.facebook.com/groups/750505538437249',
-                  'https://www.facebook.com/groups/289535414451046/']
-
-    # Muriaé >> Viçosa
-    elif origem_var.get() == 'Muriaé' and destino_var.get() == 'Viçosa':
-        print('Muriaé >> Viçosa')
-        grupos = ['https://www.facebook.com/groups/1973005072714372/', ]
-    else:
-        print('Escolha uma opção.')
-        return
-
+    global sucesso_fb
+    global sucesso_wp
     email_str = email_entry.get()
     senha_str = senha_entry.get()
 
@@ -126,9 +119,103 @@ def carona_handler():
     mensagem = "🔴 Procuro carona {0} >> {1} \n {2}, {3}." \
         .format(origem_str, destino_str, data_str.capitalize(), horario_str)
 
+    grupos_wp = []
+    grupos_fb = []
+
+    if not postagem_fb_var.get() and not postagem_wp_var.get():
+        messagebox.showerror('Algo deu errado',
+                             'Você precisa selecionar ao menos um lugar para procurar sua '
+                             'carona.')
+        return
+
+    if postagem_fb_var.get() and not email_str or not senha_str:
+        messagebox.showerror('Algo deu errado',
+                             'Você deixou alguns campos vazios, preencha-os.')
+        return
+
+    if origem_str == 'Origem' or destino_str == 'Destino' or not data_str \
+            or not horario_str or not mensagem:
+        messagebox.showerror('Algo deu errado',
+                             'Você deixou alguns campos vazios, preencha-os.')
+        return
+
+    messagebox.showinfo('Aguarde.', 'O Caronator agora irá tentar postar os pedidos de carona '
+                                    'com as configurações que você especificou, '
+                                    'pressione Ok e aguarde.')
+
+    # Muriaé >> Ouro Preto
+    if origem_var.get() == 'Muriaé' and destino_var.get() == 'Ouro Preto':
+        print('Muriaé >> Ouro Preto')
+        grupos_fb = ['https://www.facebook.com/groups/197987093613087/',
+                     'https://www.facebook.com/groups/314296848663622/',
+                     'https://www.facebook.com/groups/1487504921462576/',
+                     'https://www.facebook.com/groups/1671840656410743/']
+        grupos_wp = ['Carona: 🚗BH<>🚘<>Muriaé🚕', ]
+
+    # Ouro Preto >> Muriaé
+    elif origem_var.get() == 'Ouro Preto' and destino_var.get() == 'Muriaé':
+        print('Ouro Preto >> Muriaé')
+        grupos_fb = ['https://www.facebook.com/groups/197987093613087/',
+                     'https://www.facebook.com/groups/314296848663622/',
+                     'https://www.facebook.com/groups/1487504921462576/',
+                     'https://www.facebook.com/groups/1671840656410743/']
+        grupos_wp = ['Carona: 🚗BH<>🚘<>Muriaé🚕', ]
+
+    # Ouro Preto >> Viçosa
+    elif origem_var.get() == 'Ouro Preto' and destino_var.get() == 'Viçosa':
+        print('Ouro Preto >> Viçosa')
+        grupos_fb = ['https://www.facebook.com/groups/197987093613087/',
+                     'https://www.facebook.com/groups/148666088555002/',
+                     'https://www.facebook.com/groups/178889795534679/',
+                     'https://www.facebook.com/groups/233530423442784/']
+        grupos_wp = ['Viçosa 《=》 BHte 🚙🤝💰', 'Carona Viçosa-OP-Viçosa🚕']
+
+    # Viçosa >> Ouro Preto
+    elif origem_var.get() == 'Viçosa' and destino_var.get() == 'Ouro Preto':
+        print('Viçosa >> Ouro Preto')
+        grupos_fb = ['https://www.facebook.com/groups/197987093613087/',
+                     'https://www.facebook.com/groups/148666088555002/',
+                     'https://www.facebook.com/groups/178889795534679/',
+                     'https://www.facebook.com/groups/233530423442784/']
+        grupos_wp = ['Viçosa 《=》 BHte 🚙🤝💰', 'Carona Viçosa-OP-Viçosa🚕']
+
+    # Viçosa >> Muriaé
+    elif origem_var.get() == 'Viçosa' and destino_var.get() == 'Muriaé':
+        print('Viçosa >> Muriaé')
+        grupos_fb = ['https://www.facebook.com/groups/197987093613087/',
+                     'https://www.facebook.com/groups/750505538437249',
+                     'https://www.facebook.com/groups/289535414451046/']
+        grupos_wp = []
+
+    # Muriaé >> Viçosa
+    elif origem_var.get() == 'Muriaé' and destino_var.get() == 'Viçosa':
+        print('Muriaé >> Viçosa')
+        grupos_fb = ['https://www.facebook.com/groups/197987093613087/',
+                     'https://www.facebook.com/groups/750505538437249',
+                     'https://www.facebook.com/groups/289535414451046/']
+        grupos_wp = []
+
+    # Teste >> Teste
+    elif origem_var.get() == 'Teste' and destino_var.get() == 'Teste':
+        print('Teste')
+
     desabilitar_elementos()
-    postar_no_facebook(email_str, senha_str, grupos, mensagem)
+    if grupos_fb and postagem_fb_var.get():
+        postar_no_facebook(email_str, senha_str, grupos_fb, mensagem)
+    if grupos_wp and postagem_wp_var.get():
+        postar_no_whatsapp(grupos_wp, mensagem)
     habilitar_elementos()
+
+    if postagem_fb_var.get() and sucesso_fb and sucesso_wp and postagem_wp_var.get():
+        messagebox.showinfo('Sucesso!', 'As postagens foram feitas no Facebook e '
+                                        'Whatsapp com sucesso.')
+    elif postagem_fb_var.get() and sucesso_fb \
+            and not sucesso_wp or not postagem_wp_var.get():
+        messagebox.showinfo('Sucesso!', 'A postagem foi feita no Facebook com sucesso.')
+    elif not postagem_fb_var.get() or not sucesso_fb \
+            and sucesso_wp and postagem_wp_var.get():
+        messagebox.showinfo('Sucesso!', 'A postagem foi feita no Whatsapp com sucesso.')
+
     return
 
 
@@ -156,6 +243,8 @@ if __name__ == '__main__':
     master = Tk()
     master.title('Caronator v1')
 
+    origem_e_destino = ['Ouro Preto', 'Muriaé', 'Viçosa', 'Teste']
+
     # Elementos do formulário de login
     login_form = Frame(master, relief="sunken", borderwidth="1")
     email_label = Label(login_form, text="E-mail do Facebook:")
@@ -175,17 +264,26 @@ if __name__ == '__main__':
     origem_label = Label(config_frame, text='Cidade de Origem:')
     destino_label = Label(config_frame, text='Cidade de Destino:')
     origem_var = StringVar()
-    origem_var.set('Origem')
     destino_var = StringVar()
-    origem = OptionMenu(config_frame, origem_var, 'Ouro Preto', 'Muriaé', 'Viçosa')
+    origem = OptionMenu(config_frame, origem_var, *origem_e_destino)
     origem.config(width=str(len('Ouro Preto')))
-    destino_var.set('Destino')
-    destino = OptionMenu(config_frame, destino_var, 'Ouro Preto', 'Muriaé', 'Viçosa')
+    destino = OptionMenu(config_frame, destino_var, *origem_e_destino)
     destino.config(width=str(len('Ouro Preto')))
     data = Entry(config_frame)
     data_label = Label(config_frame, text="Data:")
     horario = Entry(config_frame)
     horario_label = Label(config_frame, text='Horário:')
+    check_label = Label(config_frame, text='Onde deseja procurar:')
+    postagem_fb_var = IntVar()
+    postagem_fb_check = Checkbutton(config_frame, text='Facebook', variable=postagem_fb_var)
+    postagem_wp_var = IntVar()
+    postagem_wp_check = Checkbutton(config_frame, text='Whatsapp', variable=postagem_wp_var)
+
+    # Adciona os valores as variáveis do tkinter
+    origem_var.set('Origem')
+    destino_var.set('Destino')
+    postagem_fb_check.select()
+    postagem_wp_check.select()
 
     # Adciona os elementos do formulário de carona à GUI
     config_frame.pack(fill='x')
@@ -197,9 +295,14 @@ if __name__ == '__main__':
     data.grid(sticky='ew', padx='5', row='3', columnspan='2')
     horario_label.grid(sticky='w', padx='5', row='4')
     horario.grid(sticky='ew', padx='5', row='5', columnspan='2')
+    check_label.grid(sticky='w', padx='5', row='6')
+    postagem_fb_check.grid(sticky='w', padx='5', row='7', column='0')
+    postagem_wp_check.grid(sticky='w', padx='5', row='7', column='1')
 
     # Cria o botão e o coloca na GUI
     procurar_botao = Button(config_frame, text='Procurar Carona!', command=carona_handler)
-    procurar_botao.grid(sticky='e', padx='5', pady='5', column='1', row='6')
+    procurar_botao.grid(sticky='e', padx='5', pady='5', column='1', row='8')
 
+    sucesso_fb = False
+    sucesso_wp = False
     mainloop()
